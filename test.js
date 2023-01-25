1a7006bd143d2c3e2849d279a7dab419cf805d0c
vaibhav-simform <vaibhav.a@simformsolutions.com>
2023-01-25 14:35:13 +0530

    feat: T015: 2 functions are added in test.js

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
-console.log("Testing started");
-console.log("testing done");
+function printTenTimes(a) {
+  for (let i = 1; i < 11; i++) {
+    console.log(a * i);
+  }
+}
+
+function printTwentyTimes(b) {
+  for (let i = 1; i < 21; i++) {
+    console.log(b * i);
+  }
+}
