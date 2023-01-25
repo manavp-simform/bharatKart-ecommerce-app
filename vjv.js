8906b16ddc9267973d28274d959d07464ba98b86
Neel Sutariya <neel.s@simformsolutions.com>
2023-01-25 16:27:54 +0530

    feat: T056: updated vjv.js file

--- vjv.js
+++ vjv.js
@@ -1,1 +1,96 @@
-console.assert(a)
+function printVjv(){
+   if(true)
+       console.log("have a nice day");
+    else
+        console.log("have a nice day");
+
+
+    console.log("have a nice day");
+
+
+
+
+    console.log("have a nice day");
+
+
+
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    console.log("have a nice day");
+    
+
+}
+
+printVjv();
