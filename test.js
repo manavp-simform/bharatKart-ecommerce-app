82ab26443f095da974df17470cbb06a5e23eecb3
Tarang-Simform <tarang.m@simformsolutions.com>
2023-01-25 15:05:18 +0530

    wip: T08_Logout_Inprogress4

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 function printTenTimes(x) {
   for (let i = 1; i < 11; i++) {
-    console.log(x - i);
+    function printTwentyTimes(b) {
+      for (let i = 1; i < 21; i++) {
+        console.log(b * i);
+      }
+    }
   }
   console.log("Printed");
 }
